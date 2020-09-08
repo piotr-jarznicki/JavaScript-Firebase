08e4e0fa61660f7ca522355eae2a120bd8192818
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-09-08 21:28:01 +0200

    Remove for loop

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const gameBoard = document.querySelector(".game-board");
 const gameTiles = 20;
-const game = [{}];
+const game = {
+  moves: 0,
+  tilesChecked: [],
+  tilesImages: [
+    "img2/item_1.png ",
+    "img2/item_2.png ",
+    "img2/item_3.png ",
+    "img2/item_4.png ",
+    "img2/item_5.png ",
+    "img2/item_6.png ",
+    "img2/item_7.png ",
+    "img2/item_8.png ",
+    "img2/item_9.png ",
+    "img2/item_10.png ",
+    "img2/item_1.png ",
+    "img2/item_2.png ",
+    "img2/item_3.png ",
+    "img2/item_4.png ",
+    "img2/item_5.png ",
+    "img2/item_6.png ",
+    "img2/item_7.png ",
+    "img2/item_8.png ",
+    "img2/item_9.png ",
+    "img2/item_10.png ",
+  ],
+};
 
 // Pomysł z obiektem, wyciągałbym id z obiektu i wstawiał wszczepiał do diva po czym porównywał idiki przy e.target
 
@@ -17,33 +42,10 @@ const tiles = [
   { id: 10, src: "img2/item_10.png " },
 ];
 
-const tilesImages = [
-  "img2/item_1.png ",
-  "img2/item_2.png ",
-  "img2/item_3.png ",
-  "img2/item_4.png ",
-  "img2/item_5.png ",
-  "img2/item_6.png ",
-  "img2/item_7.png ",
-  "img2/item_8.png ",
-  "img2/item_9.png ",
-  "img2/item_10.png ",
-  "img2/item_1.png ",
-  "img2/item_2.png ",
-  "img2/item_3.png ",
-  "img2/item_4.png ",
-  "img2/item_5.png ",
-  "img2/item_6.png ",
-  "img2/item_7.png ",
-  "img2/item_8.png ",
-  "img2/item_9.png ",
-  "img2/item_10.png ",
-];
-
 const arrIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Pomysł, żeby porównywać id i na ich podstawie sprawdzać czy mamy parę
-tilesImages.sort(function () {
-  return 0.5 - Math.random();
-});
+// tilesImages.sort(function () {
+//   return 0.5 - Math.random();
+// });
 
 arrIds.sort(function () {
   return 0.5 - Math.random();
@@ -54,14 +56,23 @@ function checkPair() {
 }
 
 const addTiles = () => {
-  for (let i = 0; i < gameTiles; i++) {
+  function sortTilesImages() {
+    const sortedTilesImages = game.tilesImages.sort((arr) => {
+      return 0.5 - Math.random();
+    });
+    return sortedTilesImages;
+  }
+
+  const sortedTilesImages = sortTilesImages();
+  sortedTilesImages.forEach((el, index) => {
     const div = document.createElement("div");
     div.classList.add("game-tile");
-    div.innerHTML = `<img src = '${tilesImages[i]}'>`;
-    div.id = `${arrIds[i]}`;
+    div.innerHTML = `<img src = '${sortedTilesImages[index]}'>`;
+    div.id = `${arrIds[index]}`;
     gameBoard.append(div);
-    div.addEventListener("click", showTiles);
-  }
+  });
 };
+
+function checkPair() {}
 
 window.onload = addTiles();

c0a5a3e1bfc0ae1af2e6f3c42d3b81b16652378c
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-09-08 20:41:50 +0200

    Add js scripts / WIP

---
+++ app.js
@@ -0,0 +1,67 @@
+const gameBoard = document.querySelector(".game-board");
+const gameTiles = 20;
+const game = [{}];
+
+// Pomysł z obiektem, wyciągałbym id z obiektu i wstawiał wszczepiał do diva po czym porównywał idiki przy e.target
+
+const tiles = [
+  { id: 1, src: "img2/item_1.png " },
+  { id: 2, src: "img2/item_2.png " },
+  { id: 3, src: "img2/item_3.png " },
+  { id: 4, src: "img2/item_4.png " },
+  { id: 5, src: "img2/item_5.png " },
+  { id: 6, src: "img2/item_6.png " },
+  { id: 7, src: "img2/item_7.png " },
+  { id: 8, src: "img2/item_8.png " },
+  { id: 9, src: "img2/item_9.png " },
+  { id: 10, src: "img2/item_10.png " },
+];
+
+const tilesImages = [
+  "img2/item_1.png ",
+  "img2/item_2.png ",
+  "img2/item_3.png ",
+  "img2/item_4.png ",
+  "img2/item_5.png ",
+  "img2/item_6.png ",
+  "img2/item_7.png ",
+  "img2/item_8.png ",
+  "img2/item_9.png ",
+  "img2/item_10.png ",
+  "img2/item_1.png ",
+  "img2/item_2.png ",
+  "img2/item_3.png ",
+  "img2/item_4.png ",
+  "img2/item_5.png ",
+  "img2/item_6.png ",
+  "img2/item_7.png ",
+  "img2/item_8.png ",
+  "img2/item_9.png ",
+  "img2/item_10.png ",
+];
+
+const arrIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Pomysł, żeby porównywać id i na ich podstawie sprawdzać czy mamy parę
+tilesImages.sort(function () {
+  return 0.5 - Math.random();
+});
+
+arrIds.sort(function () {
+  return 0.5 - Math.random();
+});
+
+function checkPair() {
+  // Porównywanie src e.targetu i src previous.e.targetu(?)
+}
+
+const addTiles = () => {
+  for (let i = 0; i < gameTiles; i++) {
+    const div = document.createElement("div");
+    div.classList.add("game-tile");
+    div.innerHTML = `<img src = '${tilesImages[i]}'>`;
+    div.id = `${arrIds[i]}`;
+    gameBoard.append(div);
+    div.addEventListener("click", showTiles);
+  }
+};
+
+window.onload = addTiles();

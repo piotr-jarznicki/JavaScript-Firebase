eff0646b95bcecb6e5652debba1945b1e02ec50e
Piotr Jarznicki <piotrjarznicki@gmail.com>
2020-09-13 19:39:25 +0200

    Finish all features, need to fix bug with css styles on li elements

--- src/ToDoList/app.js
+++ src/ToDoList/app.js
@@ -1,22 +1,31 @@
 const get = (...args) => document.querySelector(...args);
 const getAll = (...args) => document.querySelectorAll(...args);
 
-const activeTasksButton = get(".active-tasks");
-const finishedTasksButton = get(".finished-tasks");
-const deletedTasksButton = get(".deleted-tasks");
+const activeTasksTile = get(".active-tasks");
+const finishedTasksTile = get(".finished-tasks");
+const deletedTasksTile = get(".deleted-tasks");
 
 const createTaskButton = get(".btn-add-task");
-
 const createTaskInput = get(".add-task-input");
+
 const activeTasksList = get(".active-tasks-list");
 const deletedTasksList = get(".deleted-tasks-list");
+const finishedTasksList = get(".finished-tasks-list");
 
 const numberOfTasks = getAll(".task-number");
 
-const messages = getAll(".message");
+const message = get(".message");
+
+const inputContainer = get(".input-container");
+
+activeTasksTile.addEventListener("click", showActiveTasks);
+deletedTasksTile.addEventListener("click", showDeletedTasks);
+finishedTasksTile.addEventListener("click", showFinishedTasks);
 
 let activeTasks = [];
 let deletedTasks = [];
+let finishedTasks = [];
+
 const updateValues = (actionType) => {
   // Zamień to na switcha.
   if (actionType === "create") {
@@ -24,51 +33,96 @@ const updateValues = (actionType) => {
   } else if (actionType === "delete") {
     numberOfTasks[2].innerText = deletedTasks.length;
     numberOfTasks[0].innerText = activeTasks.length;
+  } else if (actionType === "finish") {
+    numberOfTasks[1].innerText = finishedTasks.length;
+    numberOfTasks[0].innerText = activeTasks.length;
+  } else if (actionType === "restore") {
+    numberOfTasks[2].innerText = deletedTasks.length;
+    numberOfTasks[0].innerText = activeTasks.length;
   }
 };
 
-const updateMessage = (actionType) => {
-  if (actionType === "add") {
-    console.log(messages);
-    messages[0].style.display = "none";
+const updateMessage = () => {
+  if (activeTasks.length > 0) {
+    message.style.display = "none";
+  } else {
+    message.style.display = "block";
   }
 };
 
 createTaskButton.addEventListener("click", createTask);
 function createTask() {
+  const maximumLetterAmount = 40;
   const inputText = createTaskInput.value;
-  if (inputText.trim().length === 0) {
-    alert("Add valid task text!");
-  } else {
-    const id = activeTasks.length;
-    activeTasks = [...activeTasks, { id: id, text: inputText }];
-    updateValues("create");
-    updateMessage("add");
-    renderTask();
-  }
+  const taskId = activeTasks.length;
+  const isInputTextValueEqual0 = inputText.trim().length === 0;
+  const isInputTextValueTooLong = inputText.trim().length > maximumLetterAmount;
+  isInputTextValueEqual0 || isInputTextValueTooLong
+    ? alert("Add valid task text!")
+    : (activeTasks = [...activeTasks, { id: taskId, text: inputText }]);
+  updateValues("create");
+  updateMessage();
+  renderTask();
+  createTaskInput.value = "";
 }
 
-const renderTask = () => {
-  if (true) {
-    activeTasksList.innerHTML = "";
-    activeTasks.forEach((task) => {
-      activeTasksList.innerHTML += `
-        <li class="task" id = "${task.id}" >
+const renderTask = (
+  tasksArray = activeTasks,
+  htmlTasksList = activeTasksList
+) => {
+  htmlTasksList.innerHTML = "";
+  tasksArray.forEach((task) => {
+    if (tasksArray === deletedTasks)
+      htmlTasksList.innerHTML += `
+        <li  class="task" id = "${task.id}" >
                     <div class="task-container ">
                         <div class="white-background">
                             <p class="task-info">${task.text}</p>
                         </div>
-                        <div class="delete-task"> <i class="fas fa-trash"></i></div>
-                        <div class="complete-task"><i class="fas fa-check"></i></div>
-                    </div> 
-    
+                        <div class="redo-task"> <i class="fas fa-redo"></i></div>
+                    </div>
+
         `;
-    });
-    const deleteTaskButtons = getAll("li.task .task-container .delete-task");
-    deleteTaskButtons.forEach((deleteButton) => {
-      deleteButton.addEventListener("click", deleteTask);
-    });
-  }
+    else if (tasksArray === finishedTasks) {
+      htmlTasksList.innerHTML += `
+      <li style="opacity:0.5; pointer-events:none" class="task" id = "${task.id}" >
+                  <div class="task-container ">
+                      <div class="white-background">
+                          <p class="task-info">${task.text}</p>
+                      </div>
+                      <div  class="delete-task"> <i class="fas fa-trash"></i></div>
+                      <div  class="complete-task"><i class="fas fa-check"></i></div>
+                  </div>
+
+      `;
+    } else {
+      htmlTasksList.innerHTML += `
+      <li class="task" id = "${task.id}" >
+                  <div class="task-container ">
+                      <div class="white-background">
+                          <p class="task-info">${task.text}</p>
+                      </div>
+                      <div class="delete-task"> <i class="fas fa-trash"></i></div>
+                      <div class="complete-task"><i class="fas fa-check"></i></div>
+                  </div>
+
+      `;
+    }
+  });
+  const deleteTaskButtons = getAll("li.task .task-container .delete-task");
+  deleteTaskButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", deleteTask);
+  });
+
+  const finishTaskButtons = getAll("li.task .task-container .complete-task");
+  finishTaskButtons.forEach((finishButton) => {
+    finishButton.addEventListener("click", finishTask);
+  });
+
+  const restoreTaskButtons = getAll("li.task .task-container .redo-task");
+  restoreTaskButtons.forEach((restoreButton) => {
+    restoreButton.addEventListener("click", restoreTask);
+  });
 };
 
 const deleteTask = (e) => {
@@ -78,9 +132,61 @@ const deleteTask = (e) => {
       return true;
     }
   });
+
   const taskIndexArr = activeTasks.splice(index, 1);
   const deletedTask = taskIndexArr.shift();
   deletedTasks.push(deletedTask);
   renderTask();
   updateValues("delete");
+  updateMessage();
 };
+
+const finishTask = (e) => {
+  const taskId = e.target.parentElement.parentElement.id;
+  const index = activeTasks.findIndex((task) => {
+    if (task.id === Number(taskId)) {
+      return true;
+    }
+  });
+  const taskIndexArr = activeTasks.splice(index, 1);
+  const finishedTask = taskIndexArr.shift();
+  finishedTasks.push(finishedTask);
+  renderTask();
+  updateValues("finish");
+};
+
+const restoreTask = (e) => {
+  const taskId = e.target.parentElement.parentElement.id;
+  const index = deletedTasks.findIndex((task) => {
+    if (task.id === Number(taskId)) {
+      return true;
+    }
+  });
+  const taskIndexArr = deletedTasks.splice(index, 1);
+  const restoredTask = taskIndexArr.shift();
+  activeTasks.push(restoredTask);
+  renderTask(deletedTasks, deletedTasksList);
+  updateValues("restore");
+};
+
+function showActiveTasks() {
+  deletedTasksList.style.display = "none";
+  activeTasksList.style.display = "block";
+  finishedTasksList.style.display = "none";
+  createTaskInput.disabled = false;
+  renderTask(activeTasks, activeTasksList);
+}
+function showDeletedTasks() {
+  activeTasksList.style.display = "none";
+  deletedTasksList.style.display = "block";
+  finishedTasksList.style.display = "none";
+  createTaskInput.disabled = true;
+  renderTask(deletedTasks, deletedTasksList);
+}
+function showFinishedTasks() {
+  activeTasksList.style.display = "none";
+  deletedTasksList.style.display = "none";
+  finishedTasksList.style.display = "block";
+  createTaskInput.disabled = true;
+  renderTask(finishedTasks, finishedTasksList);
+}
